5ad99e97b8c02a68b158a4145041091db48805ed
Christopher Patrick Taylor <christopher.taylor@gmail.com>
2025-07-09 14:00:06 -0400

    Update Biostats-1-Example-Simple-Bayes-Cards.R
    going through and reducing the number of errors to see if you're paying attention code

--- Biostats-1-Example-Simple-Bayes-Cards.R
+++ Biostats-1-Example-Simple-Bayes-Cards.R
@@ -1,46 +1,45 @@
-#### Bayes and Bayes Factors ####
 runCardBayes <- function() {
   rm(list = ls())
-  readcolor <- function() { 
-    n <- readline(prompt="Enter color of the card removed (R or B) : ")
-    n <- toupper(n)
-    if (is.na(n) || ((n != 'R') && (n != 'B') && (n != 'Q'))) {
-      n <- readcolor()
+
+  readcolor <- function() {
+    repeat {
+      n <- toupper(readline(prompt = "Enter color of the card drawn (R or B), or Q to quit: "))
+      if (n %in% c("R", "B", "Q")) return(n)
+      cat("Invalid input. Please enter R, B, or Q.\n")
     }
-    return(n)
   }
-  
+
   bayesrule <- function(prior, conditional1, conditional2) {
-    posterior <- (prior * conditional1) / ((prior * conditional1) + (1-prior * conditional2))
+    posterior <- (prior * conditional1) / ((prior * conditional1) + ((1 - prior) * conditional2))
+    return(posterior)
   }
-  
-  # set the prior probability that red has been removed from the deck
+
+  # Initial priors
   pRedRemoved <- 0.5
   pBlackRemoved <- 0.5
-  
-  conditionalRemoved <- 13/39 # conditional probability if a red suit has been removed
-  conditionalNotRemoved <- 26/39 # conditional probability if a red suit has not been removed
-  
-  # set our conditional variable
-  stopComputing <- FALSE
-  
-  while (stopComputing != TRUE) {
+
+  # Likelihoods
+  conditionalRemoved <- 13 / 39  # P(R | Red removed)
+  conditionalNotRemoved <- 26 / 39  # P(R | Black removed)
+
+  while (TRUE) {
     card_color <- readcolor()
-    if (card_color != 'Q') {
-      if (card_color == 'R') {
-        pRedRemoved <- bayesrule(pRedRemoved, conditionalRemoved, conditionalNotRemoved)
-        BlackRemoved <- bayesrule(pBlackRemoved, conditionalNotRemoved,conditionalRemoved)
-      } else {
-        pRedRemoved <- bayesrule(pRedRemoved, conditionalNotRemoved, conditionalRemoved)
-        pBlackRemoved <- bayesrule(pBlackRemoved, conditionalRemoved,conditionalNotRemoved)
-      }  
-      bayesfactor_k <- pRedRemoved / pBlackRemoved
-      print(sprintf("The probability that red was removed is %f", pRedRemoved))
-      print(sprintf("The probability that black was removed is %f", pBlackRemoved)) 
-      print(sprintf("Bayes Factor (K) for (H = red removed) is %f", bayesfactor_k))
+    if (card_color == 'Q') break
+
+    if (card_color == 'R') {
+      pRedRemoved <- bayesrule(pRedRemoved, conditionalRemoved, conditionalNotRemoved)
+      pBlackRemoved <- bayesrule(pBlackRemoved, conditionalNotRemoved, conditionalRemoved)
     } else {
-      stopComputing <- TRUE
+      pRedRemoved <- bayesrule(pRedRemoved, conditionalNotRemoved, conditionalRemoved)
+      pBlackRemoved <- bayesrule(pBlackRemoved, conditionalRemoved, conditionalNotRemoved)
     }
+
+    bayesfactor_k <- pRedRemoved / pBlackRemoved
+
+    cat(sprintf("P(Red Removed | data) = %.4f\n", pRedRemoved))
+    cat(sprintf("P(Black Removed | data) = %.4f\n", pBlackRemoved))
+    cat(sprintf("Bayes Factor (Red vs Black) = %.4f\n\n", bayesfactor_k))
   }
-  return()
+
+  cat("Session ended.\n")
 }
